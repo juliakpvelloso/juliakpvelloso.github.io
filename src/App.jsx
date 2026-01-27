db71f2bdcae849a1183140a0e3cea5146f579925
Julia Velloso <jvelloso@umich.edu>
2026-01-26 23:12:07 -0500

    use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Nextdoor from "./components/Nextdoor";
