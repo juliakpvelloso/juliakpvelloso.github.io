1294ef586112d4674344193c5547a8211e9cf59e
Julia Velloso <jvelloso@umich.edu>
2026-01-28 14:45:44 -0500

    fix about page on mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       <TopNav></TopNav>
       <section className="about-container">
   {/* Left text */}
-  <div className="item left-right">
+  <div className="item text left">
     <h1 className="display-1">I'm Julia,</h1>
     <h1 className="display-1">
       a <span className="display-blue">software engineer</span>
@@ -20,12 +20,12 @@ export default function About() {
     <img
       src={juliaPhoto}
       alt="Portrait of Julia"
-      style={{ width: "700px", height: "auto" }}
+      className="profile-photo"
     />
   </div>
 
   {/* Right text */}
-  <div className="item left-right">
+  <div className="item text">
     <p className="heading-4">About Me</p>
     <p className="body">I’m currently a senior at the University of Michigan studying computer science. Outside of class, you’ll usually find me reading, playing guitar, listening to music, or watching a Michigan football game — go blue!</p>
     <p className="heading-4">My Work</p> 
